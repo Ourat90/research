937e654861de64388d7351982a69eaa8512ee6da
Calvin Joe <calvinjoe@gmail.com>
2020-11-21 00:27:04 +0700

    create M_USER

---
+++ config/M_USER.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 20, 2020 at 05:25 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `web_x`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `M_USER`
+--
+
+CREATE TABLE `M_USER` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `M_USER`
+--
+ALTER TABLE `M_USER`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `M_USER`
+--
+ALTER TABLE `M_USER`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
